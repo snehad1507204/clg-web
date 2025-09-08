516f320912c45201b81d5f54bb6945d6580d1ab3
AbhayG9119 <140013541+AbhayG9119@users.noreply.github.com>
2025-09-08 18:16:40 +0530

    Add files via upload

--- src/App.js
+++ src/App.js
@@ -1,31 +1,17 @@
-import React from "react";
-import Navbar from "./components/Navbar";
-import LatestNews from "./components/LatestNews";
-import UpcomingEvents from "./components/UpcomingEvents";
-import NoticeBoard from "./components/NoticeBoard";
-import StudentLife from "./components/StudentLife";
-import Facilities from "./components/Facilities";
-import CollegeCampus from "./components/CollegeCampus";
-import WhyChooseUs from "./components/WhyChooseUs";
-import About from "./components/About";
-import Footer from "./components/Footer";
-import HeroSection from "./components/HeroSection"
+import React from 'react';
+import { BrowserRouter as Router } from 'react-router-dom'; // React Router v6
+import Navbar from './components/Navbar';
+import Footer from './components/Footer';
+import './styles/main.css';
 
 function App() {
   return (
-    <div>
-      <Navbar />
-      <HeroSection />
-      <LatestNews />
-      <UpcomingEvents />
-      <NoticeBoard />
-      <StudentLife />
-      <Facilities />
-      <CollegeCampus />
-      <WhyChooseUs />
-      <About />
-      <Footer />
-    </div>
+    <Router>
+      <div className="app">
+        <Navbar />
+        <Footer />
+      </div>
+    </Router>
   );
 }
 
