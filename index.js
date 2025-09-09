dd069f301928ddb5eee8bfd5a47fe4f58b7c4f79
Sneha Dixit <145841104+snehad1507204@users.noreply.github.com>
2025-09-09 16:19:09 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './styles/main.css'; // Import global styles
+import App from './App';
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
