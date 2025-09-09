5c2943ece81afa5c5654307e82b7df77b7a9cbea
AbhayG9119 <140013541+AbhayG9119@users.noreply.github.com>
2025-09-09 11:43:09 +0530

    Add files via upload

--- src/App.js
+++ src/App.js
@@ -1,7 +1,15 @@
 import React from 'react';
-import { BrowserRouter as Router } from 'react-router-dom'; // React Router v6
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Navbar from './components/Navbar';
 import Footer from './components/Footer';
+import Home from './pages/Home';
+import About from './pages/About';
+import Courses from './pages/Courses';
+import Eligibility from './pages/Eligibility';
+import Admission from './pages/Admission';
+import Faculty from './pages/Faculty';
+import FreeCourses from './pages/FreeCourses';
+import Contact from './pages/Contact';
 import './styles/main.css';
 
 function App() {
@@ -9,6 +17,19 @@ function App() {
     <Router>
       <div className="app">
         <Navbar />
+        <main className="main-content">
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/home" element={<Home />} />
+            <Route path="/about" element={<About />} />
+            <Route path="/courses" element={<Courses />} />
+            <Route path="/eligibility" element={<Eligibility />} />
+            <Route path="/admission" element={<Admission />} />
+            <Route path="/faculty" element={<Faculty />} />
+            <Route path="/free-courses" element={<FreeCourses />} />
+            <Route path="/contact" element={<Contact />} />
+          </Routes>
+        </main>
         <Footer />
       </div>
     </Router>
